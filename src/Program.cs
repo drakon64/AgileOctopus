0ff0edcb22301d45cb5fbd30269612edcaef9c05
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-09-30 23:30:16 +0100

    Remove custom scopes

--- src/Program.cs
+++ src/Program.cs
@@ -3,33 +3,24 @@ using System.Text.Json.Serialization;
 using AgileOctopus.GoogleCloud;
 using AgileOctopus.Octopus;
 
-string validFrom;
-string validTo;
+var now = DateTime.Now.AddDays(1).Date;
 
-{
-    var now = DateTime.Now.AddDays(1).Date;
+var validFrom = now.ToUniversalTime().ToString("O");
 
-    validFrom = now.ToUniversalTime().ToString("O");
+var validTo = now.Add(new TimeSpan(hours: 23, minutes: 59, seconds: 0))
+    .ToUniversalTime()
+    .ToString("O");
 
-    validTo = now.Add(new TimeSpan(hours: 23, minutes: 59, seconds: 0))
-        .ToUniversalTime()
-        .ToString("O");
-}
+var httpClient = new HttpClient();
 
-StandardUnitRates standardUnitRates;
+var standardUnitRates = (
+    await httpClient.GetFromJsonAsync<StandardUnitRates>(
+        $"https://api.octopus.energy/v1/products/AGILE-24-10-01/electricity-tariffs/E-1R-AGILE-24-10-01-A/standard-unit-rates/?period_from={validFrom}&period_to={validTo}",
+        SourceGenerationContext.Default.StandardUnitRates
+    )
+)!;
 
-{
-    var httpClient = new HttpClient();
-
-    standardUnitRates = (
-        await httpClient.GetFromJsonAsync<StandardUnitRates>(
-            $"https://api.octopus.energy/v1/products/AGILE-24-10-01/electricity-tariffs/E-1R-AGILE-24-10-01-A/standard-unit-rates/?period_from={validFrom}&period_to={validTo}",
-            SourceGenerationContext.Default.StandardUnitRates
-        )
-    )!;
-
-    httpClient.Dispose();
-}
+httpClient.Dispose();
 
 var rates = standardUnitRates.Results.Reverse().ToArray();
 var hourlyRates = new List<Rate>();
